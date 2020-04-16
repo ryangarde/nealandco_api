e384dd855044a35b3fe08bcc18b056479dcc5792
Ryan Garde <ryan@hybrain.co>
2020-04-16 17:17:23 +0800

    [*] Prevent creating property images if one of them fails

--- app/Http/Controllers/PropertyImagesController.php
+++ app/Http/Controllers/PropertyImagesController.php
@@ -16,10 +16,12 @@ class PropertyImagesController extends Controller
 
   public function store(Property $property)
   {
-    for ($i=0; $i < count(request()->image); $i++) { 
-      $path = Storage::putFile('public/property_images', request()->file('image')[$i]);
-      $data[$i] = $property->propertyImages()->create(['name' => str_replace("public/","",$path)]);
-    }
+    DB::transaction(function () {
+      for ($i=0; $i < count(request()->image); $i++) { 
+        $path = Storage::putFile('public/property_images', request()->file('image')[$i]);
+        $data[$i] = $property->propertyImages()->create(['name' => str_replace("public/","",$path)]);
+      }
+    });
 
     return $data;
   }
