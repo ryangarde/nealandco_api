77cc230c4cd25d7c2857e24f9cfed2e513316ff9
Ryan Garde <ryan@hybrain.co>
2020-04-20 00:43:47 +0800

    [!] Add property images

--- app/Http/Controllers/PropertyImagesController.php
+++ app/Http/Controllers/PropertyImagesController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Property;
 use App\PropertyImage;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 
 class PropertyImagesController extends Controller
